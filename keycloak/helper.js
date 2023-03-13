66696d79ad82d84f1105fe7cd3235a300bf027f7
mhusendev <husenrgc.96@gmail.com>
2023-03-14 00:09:42 +0700

    update helper

--- keycloak/helper.js
+++ keycloak/helper.js
@@ -56,18 +56,67 @@ function isJson(str) {
 function parseJwt (token) {
     return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
 }
-module.exports = {useTokensession, parseJwt}
 
 
-// if(token) {
-//     axios -> endpoint pay -> (token->status 401) {
-//         login
+async function getInfocustomer(req) {
+    let headers;
+    var results = {
+        status: '',
+        value: ''
+    }
+    if (req.headers.authorization != null) {
+        headers = req.headers.authorization
+        //   console.log(true)
+    }
 
-//     } else {
-//         value
-//     }
-// } else {
-//     axios ->login
-// }
 
-// chek token valid/tidak
+    if (!headers) {
+        if (!req.session) {
+            // const req_session = session && session.split(' ')[1]
+
+            res.sendStatus(401)
+
+        } else {
+            let session = await useTokensession(req)
+
+            let view = await keycloack.getInfo(req, session)
+            logs.info("lihat data user")
+            if (view.status !== 200 || view.status !== 201
+                || view.status !== 204) {
+                results.status = view.status
+                results.value = view.data
+            }
+            else {
+                results.status = view.status
+                results.value = view.data
+            }
+
+        }
+
+    } else {
+        // const req_token = headers && headers.split(' ')[1]
+        // const get_token = JSON.parse(helper.decode_base64(req_token))
+        let token = headers && headers.split(' ')[1]
+
+        let view = await keycloack.getInfo(req, token)
+        //   console.log(view)
+        if (view != null) {
+            if (view.status !== 200 || view.status !== 201
+                || view.status !== 204) {
+                results.status = view.status
+                results.value = view.data
+
+            }
+            else {
+                results.status = view.status
+                results.value = view.data
+            }
+        } else {
+            results.value = { message: "user not found" }
+            results.status = 405
+        }
+    }
+    return results
+}
+
+module.exports = {useTokensession, parseJwt,getInfocustomer}
